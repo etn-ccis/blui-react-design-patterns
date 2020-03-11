e9343b72570c05dd2653fafb74cebc0bf94c0262
daileytj <daileytj89@gmail.com>
2020-03-11 13:57:22 -0400

    clean up action list linting errors

--- src/pages/action-list.tsx
+++ src/pages/action-list.tsx
@@ -14,10 +14,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 // import './style.css';
 
-const options = [
-    'Delete',
-    'View Details',
-];
+const options = ['Delete', 'View Details'];
 
 const ITEM_HEIGHT = 48;
 
@@ -26,49 +23,49 @@ export class ActionList extends React.Component {
 
     constructor(props: any) {
         super(props);
-        let list = [];
+        const list = [];
         /*
         Here we manually create the elements in the list using createItem(). Using onAddItem() would lead to an error ("cannot call setState on an unmounted component"). In the constructor, the component has not yet finished mounting, so we are unable to modify the state via setState (it hasn't been initialized yet). The details of the setState call also include a re-render of the component, which cannot happen if the component has not finished initializing. So we have to directly instantiate the value of the list in the constructor. 
         */
-        for (var i = 0; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
             list.push(this.createRandomItem());
         }
         this.state = {
             list: list,
-            menuposition: null,
+            menuPosition: null,
             selectedIndex: null,
-            activeMenu: null
-        }
+            activeMenu: null,
+        };
     }
 
     onAddItem(): void {
-        let templist = this.state.list;
-        templist.push(this.createRandomItem());
-        this.setState({ list: templist });
+        const tempList = this.state.list;
+        tempList.push(this.createRandomItem());
+        this.setState({ list: tempList });
     }
     createItem(index: number): any {
         return { id: index, name: `Item ${index}`, details: `Item ${index} occured` };
     }
     createRandomItem(): any {
-        const int = parseInt((Math.random() * 100) + '', 10);
+        const int = parseInt(`${Math.random() * 100}`, 10);
         return this.createItem(int);
     }
 
     onMenuClick(event: any, i: number): void {
-        this.setState({ menuposition: event.currentTarget, activeMenu: i });
-    };
+        this.setState({ menuPosition: event.currentTarget, activeMenu: i });
+    }
 
     onMenuItemClick(option: any, i: number): void {
         if (option === 'Delete') {
-            let templist = this.state.list;
-            templist.splice(i, 1);
-            this.setState({ list: templist });
+            const tempList = this.state.list;
+            tempList.splice(i, 1);
+            this.setState({ list: tempList });
         }
-        this.onMenuClose()
+        this.onMenuClose();
     }
 
     onMenuClose(): void {
-        this.setState({ menuposition: null, activeMenu: null });
+        this.setState({ menuPosition: null, activeMenu: null });
     }
 
     onRemoveAll(): void {
@@ -85,15 +82,17 @@ export class ActionList extends React.Component {
 
     getEmptyComponent(): any {
         return (
-            <div style={{ paddingLeft: "10px" }}>
+            <div style={{ paddingLeft: '10px' }}>
                 <Typography variant="h4">No Items Found</Typography>
-                <Button data-cy="add-item" variant="contained" onClick={() => this.onAddItem()}>Add an Item</Button>
+                <Button data-cy="add-item" variant="contained" onClick={(): void => this.onAddItem()}>
+                    Add an Item
+                </Button>
             </div>
         );
     }
 
     render(): any {
-        const { menuposition } = this.state;
+        const { menuPosition } = this.state;
 
         return (
             <div style={{ display: 'flex' }}>
@@ -102,63 +101,76 @@ export class ActionList extends React.Component {
                         <Toolbar data-cy="pxb-toolbar">
                             <Typography variant="h6" color="inherit">
                                 Action List
-          </Typography>
+                            </Typography>
                             <div style={{ flex: '1 1 0px' }} />
-                            <IconButton data-cy="toolbar-delete"
+                            <IconButton
+                                data-cy="toolbar-delete"
                                 color="inherit"
                                 aria-label="Delete"
-                                onClick={() => { this.onRemoveAll() }} >
+                                onClick={(): void => {
+                                    this.onRemoveAll();
+                                }}
+                            >
                                 <DeleteIcon />
                             </IconButton>
-                            <IconButton data-cy="toolbar-add"
+                            <IconButton
+                                data-cy="toolbar-add"
                                 color="inherit"
-                                aria-label="add" onClick={() => { this.onAddItem() }}>
+                                aria-label="add"
+                                onClick={(): void => {
+                                    this.onAddItem();
+                                }}
+                            >
                                 <AddIcon />
                             </IconButton>
                         </Toolbar>
                     </AppBar>
-                    {this.state.list.length < 1 &&
-                        this.getEmptyComponent()
-                    }
+                    {this.state.list.length < 1 && this.getEmptyComponent()}
                     <List className="list" data-cy="list-content" style={{ paddingTop: '0px' }} component="nav">
-                        {
-                            this.state.list.map((item: any, i: number): any => {
-                                return (
-                                    <ListItem key={'item_' + i} button className={this.isSelected(i) ? 'selected' : ''} onClick={(): any => this.onSelected(i)}>
-                                        <ListItemText primary={item.name} secondary={item.details}></ListItemText>
-                                        <IconButton data-cy="action-menu"
-                                            onClick={(evt: any): any => this.onMenuClick(evt, i)}>
-                                            <MoreVertIcon />
-                                        </IconButton>
-                                    </ListItem>
-                                )
-                            })
-                        }
+                        {this.state.list.map((item: any, i: number): any => (
+                            <ListItem
+                                key={`item_${i}`}
+                                button
+                                className={this.isSelected(i) ? 'selected' : ''}
+                                onClick={(): any => this.onSelected(i)}
+                            >
+                                <ListItemText primary={item.name} secondary={item.details}></ListItemText>
+                                <IconButton data-cy="action-menu" onClick={(evt: any): any => this.onMenuClick(evt, i)}>
+                                    <MoreVertIcon />
+                                </IconButton>
+                            </ListItem>
+                        ))}
                     </List>
                     <Menu
                         id="long-menu"
-                        anchorEl={menuposition}
-                        open={Boolean(menuposition)}
+                        anchorEl={menuPosition}
+                        open={Boolean(menuPosition)}
                         onClose={this.onMenuClose.bind(this)}
                         PaperProps={{
                             style: {
                                 maxHeight: ITEM_HEIGHT * 4.5,
                                 width: 200,
                             },
-                        }}>
+                        }}
+                    >
                         {options.map((option) => (
-                            <MenuItem key={option} onClick={() => this.onMenuItemClick(option, this.state.activeMenu)}>
+                            <MenuItem
+                                key={option}
+                                onClick={(): void => this.onMenuItemClick(option, this.state.activeMenu)}
+                            >
                                 {option}
                             </MenuItem>
                         ))}
                     </Menu>
                 </div>
-                <div style={{
-                    flex: '1',
-                    borderLeft: '1px solid rgba(0, 0, 0, 0.12)',
-                    padding: '16px',
-                    minWidth: '200px'
-                }}>
+                <div
+                    style={{
+                        flex: '1',
+                        borderLeft: '1px solid rgba(0, 0, 0, 0.12)',
+                        padding: '16px',
+                        minWidth: '200px',
+                    }}
+                >
                     Some info about the action list...
                 </div>
             </div>

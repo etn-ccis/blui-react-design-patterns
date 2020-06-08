9bc06150e4a22ad79dfdc6ddb115b56db3afd8a3
daileytj <daileytj89@gmail.com>
2020-06-08 10:54:00 -0400

    Remove unused tests

--- src/pages/lists/data-list.test.tsx
+++ src/pages/lists/data-list.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DataList, unCamelCase, ObjectToList } from './data-list';
+import { DataList } from './data-list';
 import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -15,24 +15,7 @@ it('renders without crashing', () => {
     ReactDOM.render(
         <Provider store={store}>
             <DataList />
-        </Provider>,
-        div
+        </Provider>, div
     );
     ReactDOM.unmountComponentAtNode(div);
-});
-
-it('should capitalize separate the word based on Camel Case', () => {
-    expect(unCamelCase('camelCase')).toBe('Camel Case');
-});
-
-it('should display values in object as list', () => {
-    const obj = {
-        georgeWashington: 1789,
-        johnAdams: 1796,
-    };
-    const output = [
-        { key: 'georgeWashington', value: 1789 },
-        { key: 'johnAdams', value: 1796 },
-    ];
-    expect(ObjectToList(obj)).toEqual(output);
 });

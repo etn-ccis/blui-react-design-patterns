9992438db329111b595b2a0046e188a31d3feea4
daileytj <daileytj89@gmail.com>
2020-06-09 08:54:21 -0400

    Multiselect list WIP

---
+++ src/pages/lists/multiselect-list/multiselect-list.test.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MultiselectList } from './multiselect-list';
+
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { InfoListItem } from '@pxblue/react-components';
+import { Reducer } from '../../../redux/reducers';
+import { Checkbox } from '@material-ui/core';
+import { act } from 'react-dom/test-utils';
+
+Enzyme.configure({ adapter: new Adapter() });
+const store = createStore(Reducer());
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Provider store={store}>
+    <MultiselectList />
+  </Provider>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should render 10 list items by default', () => {
+  const multiselectList = mount(
+    <Provider store={store}>
+      <MultiselectList />
+    </Provider>
+  );
+  expect(
+    multiselectList
+      .find('.list')
+      .hostNodes()
+      .children(InfoListItem)
+  ).toHaveLength(10);
+});
+
+it('should add an item', () => {
+  const multiselectList = mount(
+    <Provider store={store}>
+      <MultiselectList />
+    </Provider>
+  );
+  expect(multiselectList.find('#add-item-button').hostNodes()).toHaveLength(1);
+  multiselectList
+    .find('#add-item-button')
+    .hostNodes()
+    .at(0)
+    .simulate('click');
+  expect(multiselectList.find('.list').children(InfoListItem)).toHaveLength(11);
+});
+
+// @ TODO: fix the following tests
+
+it('should remove item', () => {
+  const multiselectList = mount(
+    <Provider store={store}>
+      <MultiselectList />
+    </Provider>
+  );
+ 
+  // @TODO click checkbox or setstate of selected items directly
+  // multiselectList.find('.checkbox').hostNodes().at(0).simulate('click');
+  // eslint-disable-next-line no-console
+  // eslint-disable-next-line no-unused-expressions
+  // multiselectList.find(Checkbox).at(0).prop('checked')();
+
+  // act(() => {
+  // multiselectList.find(Checkbox).at(0).props().checked=true; 
+  multiselectList.find('.checkbox').hostNodes().at(0).simulate('change',{ target: { checked: true } });
+  expect((multiselectList).find('.checkbox').hostNodes().at(0))
+  // multiselectList.find(Checkbox).at(0).prop('onChange')({}, true);
+  // });
+
+
+  expect(multiselectList.find('#remove-items-button').hostNodes()).toHaveLength(1);
+  multiselectList
+    .find('#remove-items-button')
+    .hostNodes()
+    .at(0)
+    .simulate('click');
+  expect(multiselectList.find('.list').children(InfoListItem)).toHaveLength(9);
+});
+
+// it('should cancel selected items', () => {
+//   const multiselectList = mount(
+//     <Provider store={store}>
+//       <MultiselectList />
+//     </Provider>
+//   );
+ 
+//   // @TODO click checkbox or set state of selected items directly
+  
+//   expect(multiselectList.find('#cancel-button').hostNodes()).toHaveLength(1);
+//   multiselectList
+//     .find('#cancel-button')
+//     .hostNodes()
+//     .at(0)
+//     .simulate('click');
+//   expect(multiselectList.find('.list').children(ListItem)).toHaveLength(10);
+
+//   //  @TODO check that there are no selected items
+// });
---
+++ src/pages/lists/multiselect-list/multiselect-list.tsx
@@ -0,0 +1,203 @@
+import React, { useState } from 'react';
+import { makeStyles, createStyles, AppBar, Toolbar, Typography, List, ListItem, Checkbox, IconButton, Hidden, Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CancelIcon from '@material-ui/icons/Cancel';
+import AddIcon from '@material-ui/icons/Add'
+import MenuIcon from '@material-ui/icons/Menu';
+import ComputerIcon from '@material-ui/icons/Computer'
+import { TOGGLE_DRAWER } from '../../../redux/actions';
+import { useDispatch } from 'react-redux';
+import clsx from 'clsx';
+import { EmptyState, InfoListItem } from '@pxblue/react-components';
+
+export type ListItem = {
+  id: number;
+  name: string;
+  details: string;
+  check?: boolean;
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    footer: {
+      maxWidth: '500px',
+      position: 'fixed',
+      bottom: 0,
+      left: '50%',
+      background: '#fff',
+      transform: 'translateX(-50%, 100%)',
+      transition: 'all 0.2s cubic- bezier(0.4, 0.0, 0.2, 1)',
+      opacity: 0,
+      padding: '4px',
+      paddingLeft: '32px',
+      visibility: 'hidden',
+      boxShadow: '0px -8px 28px rgba(0, 0, 0, 0.25), 0px 10px 10px rgba(0, 0, 0, 0.22)',
+    },
+    snackbar: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      lineHeight: '36px',
+      opacity: 1,
+    },
+    active: {
+      opacity: 1,
+      transform: 'translateX(-50%, 0)',
+      visibility: 'visible',
+    }
+  })
+);
+
+export const MultiselectList = (): JSX.Element => {
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  function createItem(index: number, randomStatus: string): ListItem {
+    return {
+      id: index,
+      name: `Item ${index}`,
+      details: `Status: ${randomStatus}`
+    };
+  }
+
+  function createRandomItem(): ListItem {
+    const int = parseInt(`${Math.random() * 100}`, 10);
+    const randomStatus = Math.random() >= 0.3 ? "normal" : "alarm";
+    return createItem(int, randomStatus);
+  }
+
+  const generatedList = [];
+
+  for (let i = 0; i < 10; i++) {
+    generatedList.push(createRandomItem());
+  }
+
+  const [list, setList] = useState<ListItem[]>(generatedList);
+  const [selectedItems, setSelectedItems] = useState<any>([]);
+
+  function onSelect(item: ListItem): void {
+    if (selectedItems.indexOf(item) === -1) {
+      setSelectedItems([...selectedItems, item])
+    }
+    else {
+      const index = selectedItems.indexOf(item);
+      setSelectedItems(selectedItems.filter((_: ListItem, i: number) => i !== index));
+    }
+  };
+
+  function isSelected(item: ListItem): boolean {
+    return selectedItems.indexOf(item) !== -1;
+  }
+
+  function onAddItem(): void {
+    setList([...list, createRandomItem()]);
+  }
+
+  function onDelete(): void {
+    const updatedList = [...list];
+
+    selectedItems.forEach((item: ListItem) => {
+      const index = updatedList.indexOf(item);
+      updatedList.splice(index, 1);
+    });
+
+    setList(updatedList);
+    setSelectedItems([]);
+  }
+
+  function onCancel(): void {
+    list.forEach((item: ListItem): void => {
+      item.check = false;
+    })
+    setSelectedItems([]);
+  }
+
+  const getEmptyComponent = (): JSX.Element => (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '20px',
+        height: 'calc(100vh - 128px)',
+      }}
+    >
+      <EmptyState
+        icon={<ComputerIcon style={{ fontSize: '100px' }} />}
+        title={'No Items Found'}
+        actions={
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: '10px' }}
+            onClick={(): void => onAddItem()}
+          >
+            <AddIcon style={{ marginRight: '5px' }} />
+                    Add an Item
+                </Button>
+        }
+      />
+    </div>
+  );
+
+  return (
+    <div style={{ overflow: 'hidden' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Hidden mdUp={true}>
+            <IconButton
+              color={'inherit'}
+              onClick={(): void => {
+                dispatch({ type: TOGGLE_DRAWER, payload: true });
+              }}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginLeft: '-12px',
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+          <Typography variant="h6" data-cy="pxb-toolbar" color="inherit">
+            Multiselect list
+          </Typography>
+          <div style={{ flex: '1 1 0px' }} />
+          <IconButton
+            id="add-item-button"
+            data-cy="toolbar-add"
+            color="inherit"
+            aria-label="add"
+            onClick={(): void => {
+              onAddItem();
+            }}
+          >
+            <AddIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {list.length < 1 && getEmptyComponent()}
+      <List data-cy="list-content" className="list">
+        {list.map((item, index) => (
+          <InfoListItem key={`listItem_${index}`}
+            icon={<Checkbox className="checkbox" value={item.name}
+              onChange={(): void => onSelect(item)}
+              checked={isSelected(item)} />}
+            title={item.name}
+            subtitle={item.details} chevron> </InfoListItem>
+        ))}
+      </List>
+      <footer data-cy="snackbar" className={selectedItems.length > 0 ? clsx(classes.snackbar, classes.active, classes.footer) : clsx(classes.snackbar, classes.footer)}>
+        <Typography>{selectedItems.length} selected items</Typography>
+        <div>
+          <IconButton id="remove-items-button" onClick={onDelete} data-cy="snackbar-delete">
+            <DeleteIcon />
+          </IconButton>
+          <IconButton id="cancel-button" onClick={onCancel} data-cy="snackbar-cancel">
+            <CancelIcon />
+          </IconButton>
+        </div>
+      </footer >
+    </div >
+  )
+}

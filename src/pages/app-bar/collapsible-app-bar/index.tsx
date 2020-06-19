17fe32ad4e2eb3775c77e48f230fd87d3f29df44
huayunh <hhuayun0828@gmail.com>
2020-06-19 12:10:44 -0400

    Fix scroll issue; clean up styles

--- src/pages/app-bar/collapsible-app-bar/index.tsx
+++ src/pages/app-bar/collapsible-app-bar/index.tsx
@@ -1,41 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// Material-UI components
-
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import PersonIcon from '@material-ui/icons/Person';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import MenuIcon from '@material-ui/icons/Menu';
+import {
+    Typography,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    IconButton,
+    AppBar,
+    Hidden,
+    makeStyles,
+    Theme,
+} from '@material-ui/core';
+import { Person as PersonIcon, MoreVert as MoreVertIcon, Menu as MenuIcon } from '@material-ui/icons';
+import { Spacer } from '@pxblue/react-components';
+import clsx from 'clsx';
 import { listItems } from '../../../assets/list';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import { AppBar, Hidden } from '@material-ui/core';
-import { makeStyles, Theme } from '@material-ui/core/styles';
 import { useDispatch } from 'react-redux';
 import { TOGGLE_DRAWER } from '../../../redux/actions';
-import { Spacer } from '@pxblue/react-components';
+
+const MAX_APP_BAR_HEIGHT = 128; // Specified by Material Design
 
 const useStyles = makeStyles((theme: Theme) => ({
     banner: {
         // IE 11 does not support background blend mode. To see the image, you need to reverse the order of the image and gradient in the background property below.
         background:
-            'linear-gradient(rgba(0, 123, 193, 1), rgba(0, 75, 158, 1)),url(https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjMzODg2NTc0MDky/abraham-lincoln-9382540-2-402.jpg)',
+            'linear-gradient(rgba(0, 123, 193, 1), rgba(0, 75, 158, 1)), url(https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjMzODg2NTc0MDky/abraham-lincoln-9382540-2-402.jpg)',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'right',
         backgroundBlendMode: 'soft-light',
-        minHeight: theme.spacing(34),
+        minHeight: MAX_APP_BAR_HEIGHT,
         color: '#fff',
+        position: 'relative',
+    },
+    alignTopContent: {
+        top: -theme.spacing(8),
+        [theme.breakpoints.down('xs')]: {
+            top: -theme.spacing(7),
+        },
     },
     bannerMain: {
         flexDirection: 'column',
         alignItems: 'flex-start',
     },
     content: {
-        marginLeft: theme.spacing(8),
+        position: 'absolute',
+        left: theme.spacing(9),
+        bottom: 28,
     },
     flexCenter: {
         display: 'flex',
@@ -43,57 +55,56 @@ const useStyles = makeStyles((theme: Theme) => ({
         width: '100%',
     },
     header: {
-        top: 0,
-        left: 'auto',
-        right: 'auto',
-        transform: 'translateY(-100%)',
+        transform: 'translateY(-120%)',
         transition: 'all 0.5s ease-in-out',
     },
     top: {
         transform: 'translateY(0)',
-        top: '0',
     },
-    noPadLeft: {
-        paddingLeft: '0px',
+    headerTitle: {
+        fontWeight: 600,
+        lineHeight: '1.6rem',
+    },
+    headerSubtitle: {
+        marginTop: -2,
+        fontWeight: 300,
+        lineHeight: '1.2rem',
+    },
+    bannerActionItems: {
+        display: 'flex',
+        width: '100%',
+        minHeight: theme.spacing(8),
+        [theme.breakpoints.down('xs')]: {
+            minHeight: theme.spacing(7),
+        },
     },
 }));
 
 export const CollapsibleAppBar = (props: any): JSX.Element => {
     const classes = useStyles(props);
     const [list] = useState(listItems);
-    const [activeClass, setActiveClass] = useState('');
+    const [headerActive, setHeaderActive] = useState(false);
     const [opacity, setOpacity] = useState(1);
     const dispatch = useDispatch();
 
-    const onScrollHandler = () => {
+    const styleHeaderAndBanner = (): void => {
         setOpacity(window.pageYOffset);
-        console.log(window.pageYOffset);
         if (window.pageYOffset > 70) {
-            setActiveClass('top');
+            setHeaderActive(true);
         } else {
-            setActiveClass('');
+            setHeaderActive(false);
         }
     };
 
-    /* 
+    // When the page first gets loaded but with a pageYOffset, set the
+    // header in the appropriate position.
     useEffect(() => {
-        const scrollArea = document.getElementById('scroll-area');
-        if (scrollArea) {
-            scrollArea.addEventListener('scroll', () => {
-                setOpacity(window.pageYOffset);
-                if (window.pageYOffset > 70) {
-                    setActiveClass('top');
-                } else {
-                    setActiveClass('');
-                }
-            });
-        }
-    }, []);
-    */
+        styleHeaderAndBanner();
+    });
 
     return (
-        <div id="scroll-area" onScroll={onScrollHandler}>
-            <AppBar className={`${classes.header} ${[activeClass ? classes.top : '']}`}>
+        <div id="scroll-area" onWheel={styleHeaderAndBanner}>
+            <AppBar className={clsx(classes.header, headerActive && classes.top)} position={'sticky'}>
                 <Toolbar>
                     <Hidden mdUp>
                         <IconButton
@@ -106,22 +117,23 @@ export const CollapsibleAppBar = (props: any): JSX.Element => {
                             <MenuIcon />
                         </IconButton>
                     </Hidden>
-                    <div style={{ flex: 1 }}>
-                        <Typography variant="h6" color="inherit">
+                    <div>
+                        <Typography className={classes.headerTitle} variant="h6" color="inherit">
                             President
                         </Typography>
-                        <Typography className="sub-text" variant="body1" color="inherit">
+                        <Typography className={classes.headerSubtitle} variant="subtitle1" color="inherit">
                             Leader of the Free World
                         </Typography>
                     </div>
+                    <Spacer />
                     <IconButton color="inherit" edge={'end'}>
                         <MoreVertIcon />
                     </IconButton>
                 </Toolbar>
             </AppBar>
-            <div className={classes.banner}>
+            <div className={clsx(classes.banner, classes.alignTopContent)}>
                 <Toolbar className={classes.bannerMain}>
-                    <div style={{ display: 'flex', width: '100%', minHeight: 64 }}>
+                    <div className={classes.bannerActionItems}>
                         <Hidden mdUp>
                             <IconButton
                                 color="inherit"
@@ -138,31 +150,39 @@ export const CollapsibleAppBar = (props: any): JSX.Element => {
                             <MoreVertIcon />
                         </IconButton>
                     </div>
-                    <div className={classes.content} style={{ opacity: 1 - opacity / 200 }}>
-                        <Typography variant="h4" color="inherit">
-                            President
-                        </Typography>
-                        <Typography variant="h5" color="inherit">
-                            Commander in Chief
-                        </Typography>
-                        <Typography variant="h6" color="inherit">
-                            Leader of the Free World
-                        </Typography>
-                    </div>
                 </Toolbar>
+                <div className={classes.content} style={{ opacity: 1 - opacity / MAX_APP_BAR_HEIGHT }}>
+                    <Typography variant="h6" color="inherit">
+                        President
+                    </Typography>
+                    <Typography variant="body1" color="inherit">
+                        Commander in Chief
+                    </Typography>
+                    <Typography variant="body2" color="inherit">
+                        Leader of the Free World
+                    </Typography>
+                </div>
             </div>
-            <List component="nav">
-                {list.map((item, i) => (
-                    <ListItem key={`item-${i}`}>
-                        <ListItemIcon>
-                            <PersonIcon />
-                        </ListItemIcon>
-                        <div>
-                            <ListItemText primary={item.president} secondary={item.party} />
-                            <ListItemText className={classes.noPadLeft} secondary={item.took_office} />
-                        </div>
-                    </ListItem>
-                ))}
+            <List component="nav" className={classes.alignTopContent}>
+                {list.map(
+                    (item, i): JSX.Element => (
+                        <ListItem key={`item-${i}`}>
+                            <ListItemIcon>
+                                <PersonIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={item.president}
+                                secondary={
+                                    <>
+                                        <Typography variant={'body2'}>{item.party}</Typography>
+                                        <Typography variant={'body2'}>{item.took_office}</Typography>
+                                    </>
+                                }
+                                secondaryTypographyProps={{ component: 'div' }}
+                            />
+                        </ListItem>
+                    )
+                )}
             </List>
         </div>
     );

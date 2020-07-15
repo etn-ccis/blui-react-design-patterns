730b6b772e466e09e17e6969d38059ad6c4f6da8
Greiner <E0458886@eaton.ad.etn.com>
2020-07-15 12:48:32 -0400

    Add ids for app bar and banner

--- cypress/integration/appbar/collapsible-app-bar.spec.js
+++ cypress/integration/appbar/collapsible-app-bar.spec.js
@@ -6,9 +6,12 @@ describe('Collapsible app bar', () => {
         cy.visit('http://localhost:3000/collapsible');
     });
 
-    it('show tool bar and banner', () => {
+    it('banner displays', () => {
+        cy.get('[data-cy=banner]').should('be.visible')
+    });
+
+    it('tool bar displays on scroll', () => {
         cy.get('#scroll-area > .MuiList-root').scrollIntoView({ duration: 1000, offset:{ top: 0, left: 0 }})
-        cy.get('.MuiPaper-root > .MuiToolbar-root').should('be.visible')
-        cy.get('.makeStyles-banner-340').scrollIntoView().should('be.visible')
+        cy.get('[data-cy=app-bar] > .MuiToolbar-root').scrollIntoView().should('be.visible')
     });
 });
--- src/pages/app-bar/collapsible-app-bar/index.tsx
+++ src/pages/app-bar/collapsible-app-bar/index.tsx
@@ -97,7 +97,11 @@ export const CollapsibleAppBar = (): JSX.Element => {
             onWheel={styleHeaderAndBanner}
             style={{ backgroundColor: theme.palette.background.paper, minHeight: '100vh' }}
         >
-            <AppBar className={clsx(classes.header, headerActive && classes.top)} position={'sticky'}>
+            <AppBar
+                className={clsx(classes.header, headerActive && classes.top)}
+                data-cy={'app-bar'}
+                position={'sticky'}
+            >
                 <Toolbar>
                     <Hidden mdUp>
                         <IconButton
@@ -121,7 +125,7 @@ export const CollapsibleAppBar = (): JSX.Element => {
                 </Toolbar>
             </AppBar>
             <div className={clsx(classes.banner, classes.alignTopContent)}>
-                <Toolbar className={classes.bannerMain}>
+                <Toolbar className={classes.bannerMain} data-cy={'banner'}>
                     <div className={classes.bannerActionItems}>
                         <Hidden mdUp>
                             <IconButton

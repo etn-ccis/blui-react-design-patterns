ac98238d01f35c90bbe280772a08cd51cfaedd44
Ekta Ghag <ektasawant99@gmail.com>
2021-07-13 11:31:59 +0530

    fix PR comments

--- src/pages/loading-waiting-states/spinner-overlays/index.tsx
+++ src/pages/loading-waiting-states/spinner-overlays/index.tsx
@@ -12,11 +12,11 @@ import {
     Toolbar,
     Typography,
 } from '@material-ui/core';
-import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import { useDispatch } from 'react-redux';
 import { TOGGLE_DRAWER } from '../../../redux/actions';
-import { eulaDetails } from './data';
+import { eulaDetails } from './eulaText';
 import * as Colors from '@pxblue/colors';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -32,12 +32,12 @@ const useStyles = makeStyles((theme: Theme) => ({
     toolbarGutters: {
         padding: `0 ${theme.spacing(2)}px`,
     },
-    eulaContainerStyle: {
+    eulaContainer: {
         height: `calc(100vh - 100px)`,
         position: 'relative',
         padding: 0,
     },
-    loadingOverlayContainerStyle: {
+    loadingOverlayContainer: {
         display: 'flex',
         position: 'absolute',
         top: 0,
@@ -49,24 +49,24 @@ const useStyles = makeStyles((theme: Theme) => ({
         alignItems: 'center',
         flexDirection: 'column',
     },
-    eulaDetailsStyle: {
+    eulaDetails: {
         flex: '1 1 0',
         display: 'flex',
         flexDirection: 'column',
         height: '100%',
     },
-    eulaDetailSectionStyle: {
+    eulaDetailSection: {
         overflow: 'auto',
         display: 'flex',
         flexDirection: 'column',
         flex: '1 1 0',
         padding: `${theme.spacing(3)}px`,
     },
-    loadingContainerStyle: {
+    loadingContainer: {
         height: '100%',
         paddingTop: `${theme.spacing(1)}px`,
     },
-    overlayContainerStyle: {
+    overlayContainer: {
         position: 'absolute',
         top: 0,
         left: 0,
@@ -76,53 +76,50 @@ const useStyles = makeStyles((theme: Theme) => ({
         zIndex: 3,
         opacity: 0.2,
     },
-    btnContainerStyle: {
+    btnContainer: {
         padding: `${theme.spacing(3)}px`,
     },
-    reloadButtonStyle: {
+    reloadButton: {
         width: '100%',
     },
-    eulaContentStyle: {
+    eulaContent: {
         paddingTop: `${theme.spacing(1)}px`,
     },
-    eulaConfirmationCheckStyle: {
+    eulaConfirmationCheck: {
         padding: `0 ${theme.spacing(3)}px`,
     },
 }));
 
 export const SpinnerOverlays = (): JSX.Element => {
     const dispatch = useDispatch();
-    const theme = useTheme();
     const classes = useStyles();
-    const spinnerCirlceSize = 96;
-    const [checkboxState, setCheckboxState] = useState(false);
+    const spinnerCircleSize = 96;
+    const [eulaAccepted, setEulaAccepted] = useState(false);
     const [eulaLoaded, setEulaLoaded] = useState(false);
 
     const changeCheckboxState = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setCheckboxState(event.target.checked);
+        setEulaAccepted(event.target.checked);
     };
 
     useEffect(() => {
-        let isEulaLoaded = true;
-
-        setTimeout(() => {
-            if (isEulaLoaded) {
+        let timer: NodeJS.Timeout;
+        if (!eulaLoaded) {
+            timer = setTimeout(() => {
                 setEulaLoaded(true);
-            }
-        }, 3000);
-
+            }, 3000);
+        }
         return (): void => {
-            isEulaLoaded = false;
+            clearTimeout(timer);
         };
-    }, [checkboxState, eulaLoaded, setEulaLoaded]);
+    }, [eulaLoaded]);
 
     const reloadEulaDetails = useCallback((): void => {
         setEulaLoaded(false);
-        setCheckboxState(false);
-    }, [checkboxState, eulaLoaded, setEulaLoaded]);
+        setEulaAccepted(false);
+    }, []);
 
     return (
-        <div style={{ backgroundColor: theme.palette.background.paper, minHeight: '100vh' }}>
+        <div style={{ minHeight: '100vh' }}>
             <AppBar data-cy="pxb-toolbar" position={'sticky'}>
                 <Toolbar classes={{ gutters: classes.toolbarGutters }}>
                     <Hidden mdUp={true}>
@@ -145,43 +142,43 @@ export const SpinnerOverlays = (): JSX.Element => {
                 </Toolbar>
             </AppBar>
             <div className={classes.container}>
-                <Card className={classes.eulaContainerStyle}>
+                <Card className={classes.eulaContainer}>
                     {!eulaLoaded && (
-                        <div className={classes.loadingOverlayContainerStyle}>
-                            <CircularProgress color="secondary" size={spinnerCirlceSize} />
+                        <div className={classes.loadingOverlayContainer}>
+                            <CircularProgress color="secondary" size={spinnerCircleSize} />
                         </div>
                     )}
-                    <div className={`${classes.eulaDetailsStyle} ${!eulaLoaded ? classes.overlayContainerStyle : ''}`}>
-                        <div className={classes.eulaDetailSectionStyle}>
+                    <div className={`${classes.eulaDetails} ${!eulaLoaded ? classes.overlayContainer : ''}`}>
+                        <div className={classes.eulaDetailSection}>
                             <div>
                                 <Typography variant={'h6'}>{eulaDetails.title}</Typography>
                             </div>
                             {!eulaLoaded && (
-                                <div className={classes.loadingContainerStyle}>
+                                <div className={classes.loadingContainer}>
                                     <Typography variant="subtitle1">{eulaDetails.loadingMessage}</Typography>
                                 </div>
                             )}
-                            {eulaLoaded && <div className={classes.eulaContentStyle}>{eulaDetails.eulaContent}</div>}
+                            {eulaLoaded && <div className={classes.eulaContent}>{eulaDetails.eulaContent}</div>}
                         </div>
-                        <div className={classes.eulaConfirmationCheckStyle}>
+                        <div className={classes.eulaConfirmationCheck}>
                             <FormControlLabel
                                 control={
                                     <Checkbox
-                                        checked={checkboxState}
+                                        checked={eulaAccepted}
                                         onChange={changeCheckboxState}
                                         name="eulaConformation"
                                         color="primary"
                                     />
                                 }
-                                label={eulaDetails.eulaConfirmation}
+                                label="I have read and agree to the Terms & Conditions"
                             />
                         </div>
                         <Divider />
-                        <div className={classes.btnContainerStyle}>
+                        <div className={classes.btnContainer}>
                             <Button
                                 variant={'contained'}
                                 color={'primary'}
-                                className={classes.reloadButtonStyle}
+                                className={classes.reloadButton}
                                 onClick={reloadEulaDetails}
                             >
                                 <Typography noWrap color={'inherit'}>

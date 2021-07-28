4af66cdd3238aef0cf74bf286634f3f1e311a966
jeffvg <jeffvgreiner@eaton.com>
2021-07-28 12:35:54 -0400

    new test for RTL and setup for cypress unit test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,11 @@
  */
  module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config)
-
+  if (config.testingType === 'component') {
+    require('@cypress/react/plugins/react-scripts')(on, config)
+  }
+  // cypress codecov and cypress component test
+  
   // add other tasks to be registered here
 
   // IMPORTANT to return the config object

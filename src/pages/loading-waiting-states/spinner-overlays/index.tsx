11fe8a2d357cea3f39ac1c6c6907aef5ed3e5ed8
Ekta Ghag <ektasawant99@gmail.com>
2021-08-11 19:20:17 +0530

    revert changes

--- src/pages/loading-waiting-states/spinner-overlays/index.tsx
+++ src/pages/loading-waiting-states/spinner-overlays/index.tsx
@@ -116,7 +116,7 @@ export const SpinnerOverlays = (): JSX.Element => {
         let timer: NodeJS.Timeout;
         if (!eulaLoaded) {
             timer = setTimeout(() => {
-                setEulaLoaded(false);
+                setEulaLoaded(true);
             }, 3000);
         }
         return (): void => {

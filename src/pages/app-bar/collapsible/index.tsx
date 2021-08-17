fe6ab627c9107944b284d8df8077935b32531945
Ekta Ghag <ektasawant99@gmail.com>
2021-08-17 13:50:55 +0530

    fix css

--- src/pages/app-bar/collapsible/index.tsx
+++ src/pages/app-bar/collapsible/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge, Hidden, IconButton,Toolbar  } from '@material-ui/core';
+import { Badge, Hidden, IconButton, Toolbar, Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import HelpIcon from '@material-ui/icons/Help';
@@ -7,7 +7,10 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { AppBar, Spacer, ThreeLiner } from '@pxblue/react-components';
 
-const useStyles = makeStyles(() => ({
+const backgroundImage = require('../../../assets/collapsible_app_bar_demo.jpg').default;
+const linearGradientOverlayImage = `linear-gradient(to bottom, rgba(0, 123, 193, 1) 22.4%, rgba(0, 123, 193, 0.2) 100%), url(${backgroundImage})`;
+
+const useStyles = makeStyles((theme: Theme) => ({
     title: {},
     subtitle: {},
     info: {},
@@ -16,10 +19,9 @@ const useStyles = makeStyles(() => ({
         position: 'absolute',
         flexGrow: 1,
         marginLeft: 0,
-    },
-    linerRTL: {
-        marginLeft: 0,
-        marginRight: 56,
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: 56,
+        },
     },
     expanded: {
         '& $liner': {
@@ -44,58 +46,61 @@ const useStyles = makeStyles(() => ({
         paddingLeft: 16,
         paddingRight: 4,
     },
-    toolbarGuttersRTL: {
-        paddingLeft: 4,
-        paddingRight: 16,
+    backgroundGradient: {
+        backgroundImage: `${linearGradientOverlayImage}`,
+        backgroundSize: 'unset',
+        backgroundPosition: 'center',
     },
 }));
-const backgroundImage = require('../../../assets/collapsible_app_bar_demo.jpg').default;
 
 export const Collapsible = (): JSX.Element => {
     const classes = useStyles();
-
     return (
         <div style={{ minHeight: '100vh' }}>
             <AppBar
-            expandedHeight={200}
-            collapsedHeight={64}
-            scrollThreshold={136}
-            animationDuration={300}
-            backgroundImage={backgroundImage}
-            variant={'snap'}
-            classes={{ collapsed: classes.collapsed, expanded: classes.expanded }}
-        >
-            <Toolbar classes={{ gutters: classes.toolbarGutters }}>
-            <Hidden mdUp={true}>
-                <IconButton color={'inherit'} edge={'start'} >
-                    <MenuIcon />
-                </IconButton>
-            </Hidden>
-                <Spacer />
-                <ThreeLiner
-                    classes={{ title: classes.title, subtitle: classes.subtitle, info: classes.info }}
-                    className={classes.liner}
-                    title={'Timeline'}
-                    subtitle={'Online'}
-                    info={'Gary Steel Works'}
-                    animationDuration={300}
-                />
-                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                    <IconButton color={'inherit'}>
-                        <HelpIcon />
-                    </IconButton>
-                    <IconButton color={'inherit'}>
-                    <Badge color="error" badgeContent={88}>
-                        <NotificationsIcon />
-                    </Badge>
-                    </IconButton>
-                    <IconButton color={'inherit'}>
-                        <MoreVertIcon />
-                    </IconButton>
-                </div>
-            </Toolbar>
-        </AppBar>
-        <div style={{height:'2000px'}}></div>
+                expandedHeight={200}
+                collapsedHeight={64}
+                scrollThreshold={136}
+                animationDuration={300}
+                backgroundImage={backgroundImage}
+                variant={'snap'}
+                classes={{
+                    collapsed: classes.collapsed,
+                    expanded: classes.expanded,
+                    background: classes.backgroundGradient,
+                }}
+            >
+                <Toolbar classes={{ gutters: classes.toolbarGutters }}>
+                    <Hidden mdUp={true}>
+                        <IconButton color={'inherit'} edge={'start'}>
+                            <MenuIcon />
+                        </IconButton>
+                    </Hidden>
+                    <Spacer />
+                    <ThreeLiner
+                        classes={{ title: classes.title, subtitle: classes.subtitle, info: classes.info }}
+                        className={classes.liner}
+                        title={'Timeline'}
+                        subtitle={'Online'}
+                        info={'Gary Steel Works'}
+                        animationDuration={300}
+                    />
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <IconButton color={'inherit'}>
+                            <HelpIcon />
+                        </IconButton>
+                        <IconButton color={'inherit'}>
+                            <Badge color="error" badgeContent={88}>
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton color={'inherit'}>
+                            <MoreVertIcon />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <div style={{ height: '2000px' }}></div>
         </div>
     );
 };

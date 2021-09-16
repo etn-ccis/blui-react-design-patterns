a004292ebae4d578974391b52e3ea39d0239d18f
Ekta Ghag <ektasawant99@gmail.com>
2021-09-16 12:31:27 +0530

    fix anchorel issuw

--- src/pages/app-bar/dropdown-toolbar/index.tsx
+++ src/pages/app-bar/dropdown-toolbar/index.tsx
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AppBar, Drawer, Hidden, List, Toolbar, IconButton } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import { DropdownToolbar, InfoListItem } from '@pxblue/react-components';
-// import { useDispatch } from 'react-redux';
-// import { TOGGLE_DRAWER } from '../../../redux/actions';
-import * as colors from '@pxblue/colors';
+import { useDispatch } from 'react-redux';
+import { TOGGLE_DRAWER } from '../../../redux/actions';
 
 const menuGroups = [
     {
@@ -17,18 +16,6 @@ const menuGroups = [
     },
 ];
 
-// const bottomSheet = (): JSX.Element => (
-//     <Drawer anchor={'bottom'} open={true}>
-//         <List>
-//         {['All Locations', 'Gary Steel Works', 'US Steel'].map((text) => (
-//           <ListItem button key={text}>
-//             <ListItemText primary={text} />
-//           </ListItem>
-//         ))}
-//       </List>
-//     </Drawer>
-// );
-
 const useStyles = makeStyles((theme: Theme) => ({
     toolbarGutters: {
         padding: 0,
@@ -36,44 +23,13 @@ const useStyles = makeStyles((theme: Theme) => ({
             padding: `0px ${theme.spacing(2)}px`,
         },
     },
-    appBarContainer: {
-        maxWidth: 960,
-        margin: '0 auto',
-        padding: `0 ${theme.spacing(2)}px`,
-    },
-    appBar: {
-        marginBottom: `${theme.spacing(3)}px`,
-        '&:last-child': {
-            marginBottom: 0,
-        },
-    },
-    appBarHeader: {
-        maxWidth: 600,
-        margin: `${theme.spacing(5)}px auto ${theme.spacing(3)}px`,
-        [theme.breakpoints.down('md')]: {
-            padding: `0 ${theme.spacing(2)}px`,
-        },
-    },
-    badge: {
-        backgroundColor: colors.green[500],
-        color: colors.green[500],
-        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    },
     paper: {
         marginTop: `${theme.spacing(1)}px`,
-    },
-    subtitle: {
-        marginTop: `-${theme.spacing(0.5)}px`,
-    },
-    textContainer: {
-        marginLeft: `${theme.spacing(2.5)}px`,
-        overflow: 'hidden',
-        whiteSpace: 'nowrap',
     },
 }));
 
 export const PxbDropdownToolbar = (): JSX.Element => {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const classes = useStyles();
     const [showMenu, setShowMenu] = useState<boolean>(false);
 
@@ -86,7 +42,7 @@ export const PxbDropdownToolbar = (): JSX.Element => {
                             data-cy="toolbar-menu"
                             color={'inherit'}
                             onClick={(): void => {
-                                // dispatch({ type: TOGGLE_DRAWER, payload: true });
+                                dispatch({ type: TOGGLE_DRAWER, payload: true });
                                 setShowMenu(true)
                             }}
                             edge={'start'}
@@ -105,27 +61,10 @@ export const PxbDropdownToolbar = (): JSX.Element => {
                         <DropdownToolbar
                             title={'Appbar Title'}
                             subtitle={'Dropdown Toolbar'}
-                            /* eslint-disable-next-line no-console */
-                            // onClick={(): void => {console.log('clicked'); setShowMenu(true)}}
-                            onOpen={(): void => {
-                                /* eslint-disable-next-line no-console */
-                                console.log('clicked');
-                                setShowMenu(true)
-                            }}
-                            onClose={(): void => {
-                                /* eslint-disable-next-line no-console */
-                                console.log('clicked false');
-                                setShowMenu(false)
-                            }}
-                            
-                        >
-                            
-                        </DropdownToolbar>
-                    </Hidden>
-                    <div />
-                </Toolbar>
-            </AppBar>
-            <Drawer
+                            menu={
+                                <React.Fragment key={'bottom'}>
+
+                                <Drawer
                                     data-cy="bottom-sheet"
                                     anchor={'bottom'}
                                     transitionDuration={250}
@@ -154,6 +93,27 @@ export const PxbDropdownToolbar = (): JSX.Element => {
                                         />
                                     </List>
                                 </Drawer>
+                                </React.Fragment>
+                            }
+                            /* eslint-disable-next-line no-console */
+                            // onClick={(): void => {console.log('clicked'); setShowMenu(true)}}
+                            onOpen={(): void => {
+                                /* eslint-disable-next-line no-console */
+                                console.log('clicked');
+                                setShowMenu(true);
+                            }}
+                            onClose={(): void => {
+                                /* eslint-disable-next-line no-console */
+                                console.log('clicked false');
+                                setShowMenu(false);
+                            }}
+
+                        >
+                        </DropdownToolbar>
+                    </Hidden>
+                    <div />
+                </Toolbar>
+            </AppBar>
         </div>
     );
 };

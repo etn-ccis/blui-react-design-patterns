d2d6fc6c4101a329f2b20a04124a694ddf2f924d
jeffvg <jeffvgreiner@eaton.com>
2021-11-22 13:56:56 -0500

    add hero & actionrow button

--- src/pages/lists/action-list/action-list-local-actions/action-list-local-actions-scorecard.tsx
+++ src/pages/lists/action-list/action-list-local-actions/action-list-local-actions-scorecard.tsx
@@ -1,39 +1,80 @@
 import React from 'react';
-import { Hero, HeroBanner, ScoreCard } from '@pxblue/react-components';
-import { List, ListItem, ListItemText } from '@material-ui/core';
-// import { Theme } from '@material-ui/core/styles/createMuiTheme';
-// import makeStyles from '@material-ui/core/styles/makeStyles';
 import * as Colors from '@pxblue/colors';
-import { Temp } from '@pxblue/icons-mui';
-
-// const useStyles = makeStyles((theme: Theme) => ({
-
-// }));
+import { Hero, HeroBanner, ScoreCard } from '@pxblue/react-components';
+import { GradeA, Temp, Moisture as Humidity } from '@pxblue/icons-mui';
+import Button from '@material-ui/core/Button';
 
 const ActionListLocalActionsScoreCardRender = (): JSX.Element => (        
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div
+        style={{
+            flex: 1,
+            width: '100%',
+            maxWidth: '416px',
+            minWidth: '350px',
+            padding: '16px',
+            display: 'flex',
+            flexDirection: 'column',
+        }}
+    >
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
             <ScoreCard
-                headerColor={Colors.red[500]}
+                headerColor={Colors.white[50]}
                 headerTitle={'Overview'}
-                headerFontColor={Colors.white[50]}
+                headerFontColor={Colors.blue[500]}
                 badge={
-                    <HeroBanner>
+                    <HeroBanner style={{ minWidth: 210 }}>
                         <Hero
-                            icon={<Temp fontSize={'inherit'} htmlColor={Colors.black[500]} />}
+                            icon={
+                                <Humidity
+                                    fontSize={'inherit'}
+                                    htmlColor={Colors.blue[200]}
+                                />
+                            }
+                            label={'Humidity'}
+                            value={54}
+                            units={'%'}
+                            iconSize={48}
+                            fontSize={'normal'}
+                        />
+                        <Hero
+                            icon={
+                                <Temp
+                                    fontSize={'inherit'}
+                                    htmlColor={Colors.red[500]}
+                                />
+                            }
                             label={'Temperature'}
                             iconSize={48}
-                            value={98}
+                            value={97}
                             units={'°F'}
+                            fontSize={'normal'}
+                        />
+                        <Hero
+                            icon={
+                                <GradeA
+                                    fontSize={'inherit'}
+                                    htmlColor={Colors.green[500]}
+                                />
+                            }
+                            label={'Overall'}
+                            value={96}
+                            units={'/100'}
+                            iconSize={48}
                             fontSize={'normal'}
                         />
                     </HeroBanner>
                 }
+                badgeOffset={0}
                 actionRow={
-                    <List>
-                        <ListItem>
-                            <ListItemText primary="View Location" />
-                        </ListItem>
-                    </List>
+                    <Button variant={'contained'} color={'primary'} fullWidth={true}>
+                        Run Diagnostics{' '}
+                    </Button>
                 }
-            ></ScoreCard>
-    );
+            >
+            </ScoreCard>
+        </div>
+    </div>
+</div>
+);
     export const ActionListLocalActionsScoreCard = React.forwardRef(ActionListLocalActionsScoreCardRender);

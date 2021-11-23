c033913512e1cf28818af17d52e6390cced845ca
daileytj <daileytj89@gmail.com>
2021-11-23 11:10:47 -0500

    Refactor LanguageSelectMobile component

--- src/pages/lists/action-list/action-list-local-actions/select-language-mobile.tsx
+++ src/pages/lists/action-list/action-list-local-actions/select-language-mobile.tsx
@@ -6,45 +6,54 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 import { useTheme } from '@material-ui/styles';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 
+type LanguageData = {
+    id: string;
+    title: string;
+};
 
-const useStyles = makeStyles((theme:Theme) =>
+const languageData: LanguageData[] = [
+    {
+        id: 'deutch',
+        title: 'Deutsch',
+    },
+    {
+        id: 'english',
+        title: 'English',
+    },
+    {
+        id: 'espanol',
+        title: 'Español',
+    },
+    {
+        id: 'francais',
+        title: 'Français',
+    },
+];
+
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        container:{ backgroundColor:theme.palette.background.paper }
-
+        container: { backgroundColor: theme.palette.background.paper },
     })
 );
 
 export const LanguageSelectMobile: React.FC = () => {
-    const theme = useTheme()
+    const theme = useTheme();
     const classes = useStyles(theme);
     const [language, setLanguage] = React.useState('english');
 
     return (
         <div className={classes.container}>
-            <InfoListItem 
-            title={'Deutsch'} 
-            icon={language==='deutsch' ? <Done /> : undefined}
-            onClick={(): void => {setLanguage('deutsch')}}
-            divider={'full'}
-            />
-            <InfoListItem 
-            title={'English'} 
-            icon={language==='english' ? <Done /> : undefined}
-            onClick={(): void => {setLanguage('english')}}
-            divider={'full'}
-            />
-            <InfoListItem 
-            title={'Español'} 
-            icon={language==='espanol' ? <Done /> : undefined}
-            onClick={(): void => {setLanguage('espanol')}}
-            divider={'full'}
-            />
-            <InfoListItem 
-            title={'Français'} 
-            icon={language==='francais' ? <Done /> : undefined}
-            onClick={(): void => {setLanguage('francais')}}
-            divider={'full'}
-            />
+            {languageData.map((data) => (
+                <InfoListItem
+                    key={data.id}
+                    title={data.title}
+                    icon={language === data.id ? <Done /> : undefined}
+                    onClick={(): void => {
+                        setLanguage(data.id);
+                    }}
+                    divider={'full'}
+                />
+            ))}
         </div>
     );
 };
